b904f3195af255bb4870b5efbffff54e0fc5b648
Mike Hepburn <eformat@gmail.com>
2025-09-04 08:07:52 +1000

    🦩 initial commit 🦩

--- server-chuck.py
+++ server-chuck.py
@@ -3,7 +3,6 @@ import click
 import httpx
 import base64
 import sys
-import httpx
 
 @click.command()
 @click.option("--port", default=8085, help="Port to listen", type=int)
@@ -24,8 +23,7 @@ def main(port: int):
         try:
             response = httpx.get("https://api.chucknorris.io/jokes/random", timeout=10.0)
             response.raise_for_status()
-            #return response.json()
-            return response.json()['value'].replace('\n', '')
+            return str(response.json()['value'].replace('\n', ''))
         except httpx.HTTPError as exc:
             return {"error": "Failed to fetch Chuck Norris joke", "detail": str(exc)}
 
